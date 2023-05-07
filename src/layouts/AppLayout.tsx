b540170f771aaad94020bbeb57500507e14500b8
Lisjan Hasaj <lisjan@kutia.net>
2023-05-07 13:59:02 +0200

    Setup elements container that applies margins

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
 import cs from "classnames";
+import Header from "components/Header.tsx";
 
 export default function AppLayout(props: PropsWithChildren) {
     return (
@@ -8,9 +9,10 @@ export default function AppLayout(props: PropsWithChildren) {
                 "1024:w-[70%]",
                 "840:w-[80%]",
                 "620:w-[90%]",
-                "w-full min-h-screen",
+                "w-full min-h-screen flex flex-col",
             )}
         >
+            <Header />
             {props.children}
         </div>
     );
