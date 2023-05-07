759cf7a3b5e455f22e7c31bf7189f624d0b40956
Lisjan Hasaj <lisjan@kutia.net>
2023-05-07 20:18:35 +0200

    Update header breadcrumb links

--- src/components/ui/TextInput.tsx
+++ src/components/ui/TextInput.tsx
@@ -1,6 +1,5 @@
 import { StringExtension as String } from "lib/extensions/string.extension.ts";
 import cs from "classnames";
-import InputPlaceholder from "components/ui/InputPlaceholder.tsx";
 
 interface Props {
     className?: string;
@@ -11,8 +10,6 @@ interface Props {
 }
 
 export default function TextInput(props: Props) {
-    const hasValue = typeof props.value !== "undefined" && props.value !== "";
-
     return (
         <div
             className={cs(
@@ -20,16 +17,12 @@ export default function TextInput(props: Props) {
                 props.className,
             )}
         >
-            <InputPlaceholder
-                placeholder={props.placeholder!}
-                isFocused={hasValue}
-            />
             <input
                 value={props.value}
                 disabled={props.disabled}
+                placeholder={props.placeholder}
                 className={cs(
-                    "w-full h-full bg-transparent text-base text-gray-500 border-b-[2px] border-gray-300 group-focus-within:border-brand-dark group-focus-within:text-black group-focus-within:pb-[0.5rem] group-focus-within:pt-[1.5rem] transition-colors duration-100 ease-in-out z-0",
-                    hasValue ? "pt-[1.5rem] pb-[0.5rem] px-[1rem]" : "p-[1rem]",
+                    "w-full h-full bg-transparent text-base p-[1rem] text-gray-500 border-b-[2px] border-gray-300 group-focus-within:border-brand-dark group-focus-within:text-black transition-colors duration-100 ease-in-out z-0",
                 )}
                 onChange={(e) => {
                     const value = String.capitalize(e.target.value);
