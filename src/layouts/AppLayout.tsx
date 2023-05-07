9505586ca346dd0eeae2bf68ed285e0a8ae966b3
Lisjan Hasaj <lisjan@kutia.net>
2023-05-07 13:34:14 +0200

    Setup app layout responsive margins

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,17 @@
 import { PropsWithChildren } from "react";
+import cs from "classnames";
 
 export default function AppLayout(props: PropsWithChildren) {
-    return <div>{props.children}</div>;
+    return (
+        <div
+            className={cs(
+                "1024:w-[70%]",
+                "840:w-[80%]",
+                "620:w-[90%]",
+                "w-full min-h-screen",
+            )}
+        >
+            {props.children}
+        </div>
+    );
 }
