2bf905fb6c98554ffaa7dd1c9728732b3f23e37b
Lisjan Hasaj <lisjan@kutia.net>
2023-05-10 00:45:51 +0200

    Add responsiveness for other screens

--- src/components/home/TaskStatus.tsx
+++ src/components/home/TaskStatus.tsx
@@ -1,4 +1,5 @@
 import { STATUS_TYPES } from "lib/constants";
+import cs from "classnames";
 
 export type Status = keyof typeof STATUS_TYPES;
 
@@ -8,7 +9,12 @@ interface Props {
 
 export default function TaskStatus(props: Props) {
     return (
-        <span className="w-full px-3 py-1.5 rounded-md bg-brand-dark text-xs text-white text-center">
+        <span
+            className={cs(
+                "620:w-fit",
+                "w-full px-3 py-1.5 rounded-md bg-brand-dark text-xs text-white text-center",
+            )}
+        >
             {props.status}
         </span>
     );
--- src/components/home/TasksContainer.tsx
+++ src/components/home/TasksContainer.tsx
@@ -4,9 +4,11 @@ import TaskItem from "components/home/TaskItem";
 import cs from "classnames";
 import { Task } from "components/home/TaskItem";
 import { useTasksContext } from "lib/context/TasksContext";
+import { useMediaQuery } from "lib/hooks/useMediaQuery.tsx";
 
 export default function TasksContainer() {
     const { tasks } = useTasksContext();
+    const [aboveSmallTablet] = useMediaQuery(`(min-width: 620px)`);
 
     return (
         <div className="w-full flex-1 bg-brand-dark flex flex-col overflox-hidden rounded-t-[1.8rem] shadow-lg">
@@ -15,6 +17,7 @@ export default function TasksContainer() {
             </MainContainer>
             <MainContainer
                 className={cs(
+                    "1024:py-[1.5rem]",
                     "w-full flex-1 bg-brand-light rounded-t-[1.8rem] text-gray-500 flex justify-center py-[2.5rem]",
                     tasks && tasks.length < 1 ? "items-center" : "items-start",
                 )}
@@ -24,7 +27,20 @@ export default function TasksContainer() {
                         You have nothing to do. <br /> Go get some rest.
                     </span>
                 ) : (
-                    <div className="w-full grid grid-cols-2 gap-4">
+                    <div
+                        className={cs(
+                            "620:grid-cols-1 620:gap-2",
+                            "w-full grid grid-cols-2 gap-4",
+                        )}
+                    >
+                        {aboveSmallTablet && (
+                            <div className="w-full grid grid-cols-4 gap-2 text-base taxt-gray-500 font-medium px-[1rem]">
+                                <span className="text-start">Title</span>
+                                <span className="text-start">Description</span>
+                                <span className="text-center">Status</span>
+                                <span className="text-end">Actions</span>
+                            </div>
+                        )}
                         {tasks?.map((task: Task, index: number) => (
                             <TaskItem
                                 key={index}
